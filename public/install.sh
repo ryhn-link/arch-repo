9b3f1480dc69e77060dd8400d4a8bc1c981871af
Ryhon <ryhon0@gmail.com>
2021-02-12 13:32:51 +0100

    New install script

--- public/install.sh
+++ public/install.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-if [ $USER = root ]
+if [ $EUID = 0 ]
 then
     echo '
 
 [ryhn]
-Server = https://arch.ryhn.link/pkg/$arch' >> /etc/pacman.conf
+Server = https://arch.ryhn.link/pkg/' >> /etc/pacman.conf
 
     echo
     echo "Added [ryhn] to repo list"
-    echo "Importing GPG Keys"
     echo
 
-    gpg --recv-keys 3BC1065444B0109A
-    pacman-key --lsign-key 3BC1065444B0109A
 
     echo
     echo "Synchronizing repositories"
     echo
 
     pacman -Sy
+
+    echo
+    echo "Installing keyring"
+    echo
+
+    pacman -S ryhn-keyring
 
     echo
     echo "Done! Have a nice day!"
